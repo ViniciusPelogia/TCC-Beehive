c31773949a0e0007719d9d97fc1b890c329e4155
Gohamix <vinipelogiaofc@gmail.com>
2024-04-01 19:05:56 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import express from "express";
 import conectaNoBanco from "./config/db/configDB.js";
 
-//Conexão com Banco de Dados
-const conexao = await conectaNoBanco()
-
-conexao.on("error", (erro) =>{
-    console.error("erro de conexão", erro)
-})
-
-conexao.once("open", ()=>{
-    console.log("Conexao com o banco feito com sucesso")
-})
-
 const app = express();
 
 export default app;
